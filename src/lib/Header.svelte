120f388df8fa78250e3671b4678d78d854167b78
AlyshaBradley <bradleal02@craighead.school.nz>
2021-04-15 23:56:26 +1200

    added a logo
    - added the craighead logo in the top left corner. this is because the website is affiliated with craighead, and it gives the users a sense of familiarity.

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,8 +1,17 @@
 <header class="hero is-primary">
 	<div class="hero-body">
 		<div class="container has-text-centered">
-			<h1 class="title">Year 10 Graphics</h1>
-			<h2 class="subtitle">Cabin Design</h2>
+			<a href=".">
+				<img
+					src="static/images/craighead.png"
+					alt="Craighead Logo"
+					width="14%"
+					height="auto"
+					align="left"
+				/>
+				<h1 class="title">Year 10 Graphics</h1>
+				<h2 class="subtitle">Cabin Design</h2>
+			</a>
 		</div>
 	</div>
 </header>
